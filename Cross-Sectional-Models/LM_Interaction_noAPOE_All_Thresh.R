860355f23bf44814107f919813c4e82d26d4b578
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-08-17 12:05:44 -0500

    Add files via upload

---
+++ Cross-Sectional-Models/LM_Interaction_noAPOE_All_Thresh.R
@@ -0,0 +1,53 @@
+#Packages
+library(stringr)
+library(nlme)
+library(openxlsx)
+
+#Directory
+dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/Resilience_PRS/"
+
+#Read in data and combine
+Data_bl <- readRDS(paste0(dir,"Data/VMAP_Baseline_Data_Master.rds"))
+PRS <- readRDS(paste0(dir,"Data/VMAP_PRS_Master.rds"))
+Data_bl <- merge(Data_bl,PRS,by="ID")
+
+#Define PRS vars
+vars <- c("LOAD_1_noAPOE","LOAD_0.01_noAPOE","LOAD_0.00001_noAPOE","GLOBALRES_1_noAPOE","GLOBALRES_0.01_noAPOE","GLOBALRES_0.00001_noAPOE")
+
+#Initialize vectors
+B <- NULL
+SE <- NULL
+PVAL <- NULL
+PREDICTOR <- NULL
+MODEL <- NULL
+
+#Main effects -- Cross Sectional
+for(i in 1:length(vars)){
+  column=which(grepl(vars[i],names(Data_bl)))
+  Data_bl$PRS <- Data_bl[,column]
+  output <- lm(MEM ~ Age_bl + as.factor(Sex) + PRS*CSF.ab1.42, data=Data_bl, na.action=na.omit)
+  summary(output)
+ 
+  estimate <- summary(output)$coefficients["PRS:CSF.ab1.42", "Estimate"]
+  se <- summary(output)$coefficients["PRS:CSF.ab1.42", "Std. Error"]
+  pval <- summary(output)$coefficients["PRS:CSF.ab1.42", "Pr(>|t|)"]
+  predictor <- vars[i]
+  model <- "Amyloid Interaction: Cross-Sectional Memory"
+  
+  B<-rbind(B,estimate)
+  SE<-rbind(SE,se)
+  PVAL<-rbind(PVAL,pval)
+  PREDICTOR<-rbind(PREDICTOR,predictor)
+  MODEL<-rbind(MODEL,model)
+}
+
+#Combine
+Results <- data.frame(MODEL,PREDICTOR,B,SE,PVAL)
+rownames(Results) <- c()
+colnames(Results) <- c("Model","Predictor","BETA","SE","P")
+Results$P.FDR <- p.adjust(Results$P,method="fdr")
+
+#Write out
+write.xlsx(Results,paste0(dir,"Tables/LM_Interaction_All_Thresh_without_APOE.xlsx"))
+
+
---
+++ Cross-Sectional-Models/LM_Main_noAPOE_All_Thresh.R
@@ -0,0 +1,53 @@
+#Packages
+library(stringr)
+library(nlme)
+library(openxlsx)
+
+#Directory
+dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/Resilience_PRS/"
+
+#Read in data and combine
+Data_bl <- readRDS(paste0(dir,"Data/VMAP_Baseline_Data_Master.rds"))
+PRS <- readRDS(paste0(dir,"Data/VMAP_PRS_Master.rds"))
+Data_bl <- merge(Data_bl,PRS,by="ID")
+
+#Define PRS vars
+vars <- c("LOAD_1_noAPOE","LOAD_0.01_noAPOE","LOAD_0.00001_noAPOE","GLOBALRES_1_noAPOE","GLOBALRES_0.01_noAPOE","GLOBALRES_0.00001_noAPOE")
+
+#Initialize vectors
+B <- NULL
+SE <- NULL
+PVAL <- NULL
+PREDICTOR <- NULL
+MODEL <- NULL
+
+#Main effects -- Cross Sectional
+for(i in 1:length(vars)){
+  column=which(grepl(vars[i],names(Data_bl)))
+  Data_bl$PRS <- Data_bl[,column]
+  output <- lm(MEM ~ Age_bl + as.factor(Sex) + PRS, data=Data_bl, na.action=na.omit)
+  summary(output)
+ 
+  estimate <- summary(output)$coefficients["PRS", "Estimate"]
+  se <- summary(output)$coefficients["PRS", "Std. Error"]
+  pval <- summary(output)$coefficients["PRS", "Pr(>|t|)"]
+  predictor <- vars[i]
+  model <- "Main Effects: Cross-Sectional Memory"
+  
+  B<-rbind(B,estimate)
+  SE<-rbind(SE,se)
+  PVAL<-rbind(PVAL,pval)
+  PREDICTOR<-rbind(PREDICTOR,predictor)
+  MODEL<-rbind(MODEL,model)
+}
+
+#Combine
+Results <- data.frame(MODEL,PREDICTOR,B,SE,PVAL)
+rownames(Results) <- c()
+colnames(Results) <- c("Model","Predictor","BETA","SE","P")
+Results$P.FDR <- p.adjust(Results$P,method="fdr")
+
+#Write out
+write.xlsx(Results,paste0(dir,"Tables/LM_Main_All_Thresh_without_APOE.xlsx"))
+
+
